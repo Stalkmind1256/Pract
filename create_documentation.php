f5118f8c1d92316363dd355b0fba278809968c7b
Stalkmind1256 <119118253+Stalkmind1256@users.noreply.github.com>
2023-10-22 22:07:08 +0500

    Update create_documentation.php
    pre-finaly

--- create_documentation.php
+++ create_documentation.php
@@ -1,44 +1,67 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    $host = "localhost";
-    $port = "5432";
-    $dbname = "postgres";
-    $user = "postgres";
-    $password = "123456789";
+$host = "localhost";
+$port = "5432";
+$dbname = "postgres";
+$user = "postgres";
+$password = "123456789";
 
-    $connect = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+$connect = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
-    if(!$connect){
-        die("Ошибка: Не удалось подключиться к базе данных");
-    }
+if (!$connect) {
+  die("Ошибка: Не удалось подключиться к базе данных");
+}
 
-    $title = "";
-    $content = "";
+$surname = isset($_POST['surname']) ? $_POST['surname'] : '';
+$title = isset($_POST['title']) ? $_POST['title'] : '';
+$resalt = isset($_POST['resalt']) ? $_POST['resalt'] : '';
+$marks = isset($_POST['marks']) ? $_POST['marks'] : '';
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $title = $_POST['title'];
-        $content = $_POST['content'];
-    }
 
-    $query = "INSERT INTO public.documentation(title,content) VALUES($1, $2)";
-    $params = array($title, $content);
-    $res = pg_query_params($connect, $query, $params);
-    
-    if ($res) {
-        $response = array("success" => true);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $surname = $_POST['surname'];
+  $title = $_POST['title'];
+  $resalt = $_POST['resalt'];
+  $marks = $_POST['marks'];
+
+  // Подготовка и выполнение запроса вставки данных в таблицу Students
+  $queryStudents = "INSERT INTO public.students(surname) VALUES ($1) RETURNING id";
+  $resultStudents = pg_query_params($connect, $queryStudents, array($surname));
+
+  if ($resultStudents) {
+    $studentId = pg_fetch_result($resultStudents, 0, 0);
+
+  
+
+
+    // Подготовка и выполнение запроса вставки данных в таблицу Documentation с указанием foreign key
+    $queryDocumentation = "INSERT INTO public.documentation(title, id_student, resalt, marks) VALUES ($1, $2, $3,$4)";
+    $paramsDocumentation = array($title, $studentId, $resalt,$marks);
+    $resultDocumentation = pg_query_params($connect, $queryDocumentation, $paramsDocumentation);
+
+    if ($resultDocumentation) {
+      $response = array("success" => true);
     } else {
-        $response = array("success" => false);
+      $response = array("success" => false);
     }
+  } else {
+    $response = array("success" => false);
+  }
 
-    $error = pg_last_error($connect);
-    if($error){
-        $response["error"] = "Ошибка: " . $error;
-    }
+  // Получение ошибки по запросу
+  $error = pg_last_error($connect);
+  if ($error) {
+    $response["error"] = "Ошибка: " . $error;
+  }
 
-    header('Content-Type: application/json');
-    echo json_encode($response);
-    pg_close($connect);
+  // Закрытие соединения с базой данных
+  pg_close($connect);
+
+  // Отправка ответа в формате JSON
+  header('Content-Type: application/json');
+  echo json_encode($response);
+}
 ?>
