8fe8736a8a8a833da6d66747be6ea04079f69b5b
Stalkmind1256 <119118253+Stalkmind1256@users.noreply.github.com>
2023-10-24 23:08:46 +0500

    1.4

--- documentation_list.php
+++ documentation_list.php
@@ -23,8 +23,19 @@
         die("Ошибка: Не удалось подключиться к базе данных");
     }
 
-    // Retrieving the documentation list from the database
-    $query = "SELECT s.surname, d.title, d.resalt, d.marks, d.dated FROM students s INNER JOIN documentation d ON s.id = d.id_student";
+    $surnameFilter = isset($_POST['surname']) ? $_POST['surname'] : '';
+    $dateFilter = isset($_POST['date']) ? $_POST['date'] : '';
+
+    $query = "SELECT s.surname, d.title, d.resalt, d.marks, d.dated FROM students s INNER JOIN documentation d ON s.id = d.id_student WHERE 1=1";
+
+    if (!empty($surnameFilter)) {
+        $query .= " AND s.surname = '$surnameFilter'";
+    }
+
+    if (!empty($dateFilter)) {
+        $query .= " AND d.dated = '$dateFilter'";
+    }
+
     $result = pg_query($connect, $query);
 
     if (!$result) {
@@ -33,45 +44,42 @@
 
     $documentationList = pg_fetch_all($result);
 
-    $error = pg_last_error($connect);
-    if ($error) {
-        die("Ошибка при выполнении запроса: " . $error);
+    echo "<form method='post'>";
+    echo "<input type='text' name='surname' placeholder='Введите фамилию'>";
+    echo "<input type='date' name='date' placeholder='Выберите дату'>";
+    echo "<input type='submit' value='Фильтровать'>";
+    echo "</form>";
+
+    echo "<div style='width: 100%'>";
+    echo "<table class='table-width'>";
+    echo "<colgroup>";
+    echo "<col style='width: 20%;'>";
+    echo "<col style='width: 20%;'>";
+    echo "<col style='width: 20%;'>";
+    echo "<col style='width: 20%;'>";
+    echo "<col style='width: 20%;'>";
+    echo "</colgroup>";
+    echo "<tr>";
+    echo "<th>Фамилия</th>";
+    echo "<th>Тип работы</th>";
+    echo "<th>Результат работы</th>";
+    echo "<th>Оценка</th>";
+    echo "<th>Дата</th>";
+    echo "</tr>";
+
+    foreach ($documentationList as $row) {
+        echo "<tr>";
+        echo "<td>" . $row['surname'] . "</td>";
+        echo "<td>" . $row['title'] . "</td>";
+        echo "<td>" . $row['resalt'] . "</td>";
+        echo "<td>" . $row['marks'] . "</td>";
+        echo "<td>" . $row['dated'] . "</td>";
+        echo "</tr>";
     }
 
+    echo "</table>";
+    echo "</div>";
 
-    // Displaying the documentation list
-        echo "<div style='width: 100%'>";
-        echo "<table class='table-width'>";
-        echo "<colgroup>";
-        echo "<col style='width: 20%;'>";
-        echo "<col style='width: 20%;'>";
-        echo "<col style='width: 20%;'>";
-        echo "<col style='width: 20%;'>";
-        echo "<col style='width: 20%;'>";
-        echo "</colgroup>";
-        echo "<tr>";
-        echo "<th>Фамилия</th>";
-        echo "<th>Тип работы</th>";
-        echo "<th>Результат работы</th>";
-        echo "<th>Оценка</th>";
-        echo "<th>Дата</th>";
-        echo "</tr>";
-
-        foreach ($documentationList as $row) {
-            echo "<tr>";
-            echo "<td>" . $row['surname'] . "</td>";
-            echo "<td>" . $row['title'] . "</td>";
-            echo "<td>" . $row['resalt'] . "</td>";
-            echo "<td>" . $row['marks'] . "</td>";
-            echo "<td>" . $row['dated'] . "</td>";
-            echo "</tr>";
-        }
-
-        echo "</table>";
-        echo "</div>";
-
-
-    // Closing the database connection
     pg_close($connect);
 ?>
 </body>
